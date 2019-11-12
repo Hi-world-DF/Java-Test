3a1098f33a2e4294415d0cd73a9a016b45afe538
Ernest <zerotohero1996@163.com>
2019-11-12 17:10:44 +0800

    Add files via upload

---
+++ src/main/java/com/springmvc/dao/UserMapper.java
@@ -0,0 +1,20 @@
+package com.springmvc.dao;
+
+import com.springmvc.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    public User checkUser(String name);
+
+}
