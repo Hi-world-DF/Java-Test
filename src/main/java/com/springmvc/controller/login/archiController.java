776c5f93ab9c0e9a933324e406aa4aa74b3e0bd8
Ernest <zerotohero1996@163.com>
2019-11-15 14:40:16 +0800

    Add files via upload

---
+++ src/main/java/com/springmvc/controller/login/archiController.java
@@ -0,0 +1,25 @@
+package com.springmvc.controller.login;
+
+import com.springmvc.entity.User;
+import com.springmvc.service.UserService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+@Controller
+@RequestMapping("archi")
+public class archiController {
+
+    @RequestMapping("/gotoArchiManage")
+    public ModelAndView gotoUserManage(HttpServletRequest request){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("userManage/archiList");
+        return modelAndView;
+    }
+
+}
